7427366fe254e21c5d70bbffbcca5aa82be281b5
Daffa Yudisa Akbar <daffayudisa09@gmail.com>
2024-02-28 14:52:15 +0700

    feat: add Controller and Route

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return 'NIM : 2241720008 <br> Nama : Daffa Yudisa';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,22 +21,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
 
 Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () {
-    return 'NIM : 2241720008 <br> Nama : Daffa Yudisa Akbar';
-});
+// Route::get('/about', function () {
+//     return 'NIM : 2241720008 <br> Nama : Daffa Yudisa Akbar';
+// });
 
 // Route::get('/user/{name}', function ($name) {
 //     return 'Nama Saya ' . $name;
@@ -39,10 +45,27 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-".$commentId;
 });
 
-Route::get('/route/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID '.$id;
-});
+// Route::get('/route/articles/{id}', function ($id) {
+//     return 'Halaman Artikel dengan ID '.$id;
+// });
 
 Route::get('/user/{name?}', function ($name='John') {
     return 'Nama Saya ' . $name;
 });
+
+#controller
+Route::get('/hello', [WelcomeController::class,'hello']);
+// Route::get('/', [PageController::class,'index']);
+// Route::get('/about', [PageController::class,'about']);
+// Route::get('/articles/{id}', [PageController::class,'articles']);
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
+
+Route::resource ('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
